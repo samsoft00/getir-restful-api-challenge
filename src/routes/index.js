457879f054cfb1cbd7ff2bad0027c9c56fa91807
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2021-01-18 07:55:15 +0100

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,13 @@ import GetirRoute from './getit.routes'
 
 const routes = Router()
 
+routes.get('/', (req, res) => {
+  return res.status(200).json({
+    code: 0,
+    msg: 'Getir v1.0 RESTful api challenge, go to /v1/getir-records to get started.'
+  })
+})
+
 routes.use('/v1/', GetirRoute)
 
 export default routes
